4c4b319982f95e10655192d2bd1f7677bb718198
Konstantin Belov <to@kostyabelov.ru>
2015-10-09 16:04:35 +0300

    BARO, ACCEL, GYRO, Compass completed

--- Ardroquad.ino
+++ Ardroquad.ino
@@ -3,14 +3,14 @@
 #include "LED.h"
 #include "BMP180.h"
 #include "MPU6050.h"
-#include "MPU6050HMC5883L.h"
+#include "HMC5883.h"
 
 #define _DEBUG_
 
 sensors::barometer::BMP180 barometer;
 sensors::orientation::accelerometer::MPU6050 accelerometer;
 sensors::orientation::gyroscope::MPU6050 gyroscope;
-sensors::orientation::magnetometer::MPU6050HMC5883L magnetometer;
+sensors::orientation::magnetometer::HMC5883 magnetometer;
 
 void setup() {
   Serial.begin(115200);
@@ -20,6 +20,7 @@ void setup() {
   accelerometer.initialize();
   gyroscope.initialize();
   magnetometer.initialize();
+  sensors::orientation::magnetometer::MPU6050::bypass_init();
 }
 
 void loop() {
---
+++ HMC5883.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include "I2C.h"
+#include "HMC5883.h"
+
+namespace sensors {
+namespace orientation {
+namespace magnetometer {
+
+namespace MPU6050 {
+
+void bypass_init() {
+  I2C::write_8(__I2C_address, __register_user_control, __value_user_control);
+  I2C::write_8(__I2C_address, __register_interruption_pin_config, __value_interruption_pin_config);
+  I2C::write_8(__I2C_address, __register_pwr_mgmt_1, __value_pwr_mgmt_1);
+}
+
+}
+
+const char* HMC5883::__id = "MPU6050_HMC5883L/magnetometer";
+
+void HMC5883::initialize() {
+  I2C::write_8(__I2C_address, __register_mode, __value_mode);
+}
+
+const char* HMC5883::id() const {
+  return __id;
+}
+
+void HMC5883::update() {
+  I2C::read_16(__I2C_address, __register_X, _values.X);
+  I2C::read_16(__I2C_address, __register_Y, _values.Y);
+  I2C::read_16(__I2C_address, __register_Z, _values.Z);
+}
+
+}
+}
+}
